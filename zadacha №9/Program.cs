4c581898cb0cca1242d9af33778f36ad89c3e488
ivan <suhonekov-ia>
2022-12-23 12:48:33 +0300

    Добавьте файлы проекта.

---
+++ zadacha №9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace zadacha__9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] massive = Process.GetProcesses();
+            int max = (massive.Count() - 1);
+            int pos = 0; 
+            ConsoleKeyInfo a = Console.ReadKey();
+            foreach (Process s in massive)
+            {
+                Console.WriteLine("  " + s.ProcessName);
+            }
+            while(a.Key != ConsoleKey.Enter)
+            {
+                if(a.Key == ConsoleKey.UpArrow)
+                {
+                    pos--;
+                    Console.SetCursorPosition(0, pos + 1);
+                }
+                if(a.Key == ConsoleKey.DownArrow)
+                {
+                    pos++;
+                    Console.SetCursorPosition(0, pos - 1);
+                }
+                if(pos < 0)
+                {
+                    pos++;
+                    Console.SetCursorPosition(0, pos);
+                }
+                if(pos > max)
+                {
+                    pos--;
+                    Console.SetCursorPosition(0, pos);
+                }
+                Console.WriteLine("  ");
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("->");
+                a = Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("Это все что я смог сделать");
+            
+            
+           
+       
+
+        }
+    }
+}
